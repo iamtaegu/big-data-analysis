b98615652e8f7d1f67b1f746dca274d7f4b03c28
Taegu <gmdals45@gmail.com>
2023-09-27 20:17:47 +0900

    2023-09-27 update news_contents_crawler

--- news-crawler/news_contents_crawler.py
+++ news-crawler/news_contents_crawler.py
@@ -8,8 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 
 import warnings
-warnings.filterwarnings("ignore", category=PythonDeprecationWarning)
-
+warnings.filterwarnings("ignore")
 
 def fetch_news_contents(msg):
 #    print(msg)
