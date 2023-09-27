46719f32df74b6bf2af770f3f57cc2acd4011e2b
Taegu <gmdals45@gmail.com>
2023-09-27 19:44:30 +0900

    2023-09-27 update news_contents_crawler

--- news-crawler/news_contents_crawler.py
+++ news-crawler/news_contents_crawler.py
@@ -2,11 +2,27 @@ import pdb
 import time
 import boto3
 import datetime as dt
+import json
+import requests
+
+from bs4 import BeautifulSoup
 
 def fetch_news_contents(msg):
 #    print(msg)
-    print(msg.message_id)
-    print(msg.body)
+    #print(msg.message_id)
+    #print(msg.body)
+
+    item = json.loads(msg.body)
+    #print(item)
+
+    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+
+    resp = requests.get(item['url'], headers=headers)
+
+    if resp.status_code != 200:
+        return None
+
+    soup = BeautifulSoup(resp.text, 'html.parse')
 
     pdb.set_trace()
     pass
