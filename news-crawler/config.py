6f6dbfcdd67fcfc483b6031a1a6963d25c237952
iamtaegu <gmdals45@gmail.com>
2023-10-12 08:52:10 +0900

    2023-10-12 update config

--- news-crawler/config.py
+++ news-crawler/config.py
@@ -1,4 +1,4 @@
-ELASTICSEARCH_URL = 'https://search-bigdata-da7kuvvv7sxbbwhfws5hd5zcty.us-east-1.es.amazonaws.com'
+ELASTICSEARCH_URL = ''
 ELASTICSEARCH_ID = ''
 ELASTICSEARCH_PW = ''
 ELASTICSEARCH_AUTH = (ELASTICSEARCH_ID, ELASTICSEARCH_PW)
