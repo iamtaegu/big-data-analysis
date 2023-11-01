e874bc0cd408b5a8a8d7cdb7780d061c8277f27c
Taegu <gmdals45@gmail.com>
2023-11-01 21:44:34 +0900

    2023-11-01 upd serverless

--- news-api-server/handler.py
+++ news-api-server/handler.py
@@ -1,7 +1,7 @@
 import json
 
 
-def main(event, context):
+def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
--- news-api-server/services/news_trends.py
+++ news-api-server/services/news_trends.py
@@ -1,8 +1,66 @@
 import json
+import requests
+import pandas as pd
+from config import *
+
+def query_news_trends(search):
+    query = {
+        "size": 0,
+        "aggs": {
+            "group_by_date": {
+                "date_histogram": {
+                    "field": "created_at",
+                    "interval": "day"
+                }
+            }
+        }
+    }
+    
+    if search:
+        query['query'] = {
+            "match": {
+                "title": search
+            }
+        }
+
+    query = json.dumps(query)
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    resp = requests.get(
+        f"{ELASTICSEARCH_URL}/news/_search",
+        headers=headers,
+        data = query,
+        auth = ELASTICSEARCH_AUTH,
+    )
+
+    results = resp.json()
+
+    buckets = results['aggregations']['group_by_date']['buckets']
+
+    # if len[buckets] == 0:
+    #     return []
+    
+    df = pd.DataFrame(buckets)
+    df['date'] = df['key_as_string'].str[:10]
+    df = df[['date', 'doc_count']]
+
+    return df.to_dict(orient='records')
 
 def main(event, context):
+    params = event.get('queryStringParameters')
+
+    if params:
+        search = params.get('search')
+    else:
+        search = None
+
+    trends = query_news_trends(search)
+
     body = {
-        'message': 'news trends'        
+        'message': trends        
     }
 
     response = {
@@ -15,3 +73,5 @@ def main(event, context):
             "Access-Control-Allow-Credentials": True,
         }
     }
+
+    return response
