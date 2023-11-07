9ee9ab0964f73d883772ae10f0d6ab679eb049b0
Taegu <gmdals45@gmail.com>
2023-11-07 22:26:02 +0900

    2023-11-07 add service.news_searchs

---
+++ news-api-server/services/news_searchs.py
@@ -0,0 +1,60 @@
+import json
+import requests
+import pandas as pd
+from config import *
+
+def query_news_searchs(params):
+    query = {
+        "query": {
+            "bool": {
+                "must": []
+            }
+        }
+    }
+
+    if params:
+        for key, value in params.items():
+            query["query"]["bool"]["must"].append({
+                "match": {
+                    key: value
+                }
+            })
+
+    query = json.dumps(query)
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    resp = requests.get(
+        f"{ELASTICSEARCH_URL}/news/_search",
+        headers=headers,
+        data = query,
+        auth = ELASTICSEARCH_AUTH,
+    )
+
+    results = resp.json()
+
+    return results
+
+def main(event, context):
+    params = event.get('queryStringParameters')
+
+    bodys = query_news_searchs(params)
+
+    body = {
+        'message': bodys
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        # CORS
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True,
+        }
+    }
+
+    return response
