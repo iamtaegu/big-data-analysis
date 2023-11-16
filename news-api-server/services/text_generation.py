e781f3256f78e8567a4872583b73248808e87472
Taegu <gmdals45@gmail.com>
2023-11-16 20:48:13 +0900

    2023-11-16 add text_generation

---
+++ news-api-server/services/text_generation.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from config import *
+from transformers import pipeline
+
+def req_text_generation(params):
+
+    if params:
+
+        # gpt2 다운 받음
+        generator = pipeline('text-generation', model="gpt2")
+
+        text = params.get('contents')
+
+        return generator(text, max_length=128)
+    
+def main(event, context):
+    params = event.get('queryStringParameters')
+
+    bodys = req_text_generation(params)
+
+    body = {
+        'message': bodys
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        # CORS
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True,
+        }
+    }
+
+    return response
