0a357e38887eba5ff49c021febb92dffad239dcd
iamtaegu <gmdals45@gmail.com>
2023-11-17 15:45:59 +0900

    2023-11-17 add text_generation

--- news-api-server/services/text_generation.py
+++ news-api-server/services/text_generation.py
@@ -26,7 +26,6 @@ def main(event, context):
     response = {
         "statusCode": 200,
         "body": json.dumps(body),
-        # CORS
         "headers": {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
