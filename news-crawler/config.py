8503181720838592db7189e2168538ed61552ca4
Taegu <gmdals45@gmail.com>
2023-11-18 17:07:07 +0900

    2023-11-18 upd config

--- news-crawler/config.py
+++ news-crawler/config.py
@@ -1,5 +1,5 @@
 # Domain endpoint
-ELASTICSEARCH_URL = 'https://search-bigdata-da7kuvvv7sxbbwhfws5hd5zcty.us-east-1.es.amazonaws.com'
-ELASTICSEARCH_ID = 'xorjs45'
-ELASTICSEARCH_PW = 'Rlfhrgo1!!'
+ELASTICSEARCH_URL = ''
+ELASTICSEARCH_ID = ''
+ELASTICSEARCH_PW = ''
 ELASTICSEARCH_AUTH = (ELASTICSEARCH_ID, ELASTICSEARCH_PW)
