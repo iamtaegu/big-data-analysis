d8bd154a5444a2afefe9f88519626304baca2ace
iamtaegu <gmdals45@gmail.com>
2023-12-04 15:34:39 +0900

    2023-12-04 UPD trends_aggs_date

--- news-api-server/services/trends_aggs_date.py
+++ news-api-server/services/trends_aggs_date.py
@@ -23,6 +23,8 @@ def query_news_searchs(params):
         }
     }
 
+    targetQuery = ''
+
     # SELECT * FROM TABLE WHERE key like '%value%'
     # AND (created_at >= value AND created_at < date)
     if params:
@@ -54,6 +56,8 @@ def query_news_searchs(params):
                 })
             elif "sentiment" == key:
 
+                targetQuery = "sentiment"
+
                 query['aggs']['group_by_date']['aggs'] = {
                     "group_by_sentiment": {
                         "terms": {
@@ -78,9 +82,9 @@ def query_news_searchs(params):
                     }
                 })
 
-    print(query)
-
     query = json.dumps(query)
+
+    print(query)
 
     headers = {
         'Content-Type': 'application/json'
@@ -93,9 +97,63 @@ def query_news_searchs(params):
         auth = ELASTICSEARCH_AUTH,
     )
 
+    # 클라이언트에서 사용하기 편하게
+    # 응답값 변환
     results = resp.json()
+    buckets = results['aggregations']['group_by_date']['buckets']
+    df = pd.DataFrame()
+    if "sentiment" == targetQuery:
+        df = convert_sentiment_trends(buckets)
+    elif "title" == targetQuery:
+        df = convert_news_trends(buckets)
+    else:
+        df
 
-    return results
+    return df.to_dict(orient='records')
+
+def convert_news_trends(buckets):
+    df = pd.DataFrame(buckets)
+    df['date'] = df['key_as_string'].str[:10]
+    df = df[['date', 'doc_count']]
+
+    return df
+
+def convert_sentiment_trends(buckets):
+
+    buffer = []
+
+    # 중첩된 데이터를 한 줄로 변경
+    for x in buckets:
+
+        sents = x['group_by_sentiment']['buckets']
+
+        entry = {t['key']: t['doc_count'] for t in sents}
+        entry['date'] = x['key_as_string']
+
+        buffer.append(entry)
+
+    if len(buffer)  == 0:
+        return []
+
+    df = pd.DataFrame(buffer)
+    df['date'] = df['date'].str[:10]
+
+    df = df.set_index('date')
+    df = df.fillna(0)
+
+    # 컬럼이 존재하지 않을 경우를 처리
+    if 'positive' not in df.columns:
+        df['positive'] = 0
+    if 'negative' not in df.columns:
+        df['negative'] = 0
+
+    df['sentiment'] = \
+        (df['positive'] - df['negative']) / df.sum(axis=1)
+
+    df = df.dropna()
+    df = df.reset_index()
+
+    return df
 
 def main(event, context):
     params = event.get('queryStringParameters')
